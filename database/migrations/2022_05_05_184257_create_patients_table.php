e45d56c256b8153b30346664e9cc178cf851c6ff
Gift JR <muyembegift@gmail.com>
2022-05-05 21:42:03 +0200

    #2 Ground works on patient management.

--- database/migrations/2022_05_05_184257_create_patients_table.php
+++ database/migrations/2022_05_05_184257_create_patients_table.php
@@ -15,7 +15,23 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->primary('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->mediumText('address');
+            $table->string('contactnumber');
+            $table->string('sex');
+            $table->date('dob');
+            $table->string('birth_place')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('religion')->nullable
+            $table->string('guardian')->nullable();
+            $table->string('guardian_address')->nullable();
+            $table->string('occupation');
+            $table->string('nrc',15)->nullable()->unique();();
+            $table->string('image')->default('dist/img/avatar.png');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
