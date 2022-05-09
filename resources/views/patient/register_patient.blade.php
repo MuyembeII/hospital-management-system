1afbd3785a712633462fa1fb53230a35599e401a
Thandiwe998 <78383719+Thandiwe998@users.noreply.github.com>
2022-05-09 21:51:34 +0100

    #2 formatting

--- resources/views/patient/register_patient.blade.php
+++ resources/views/patient/register_patient.blade.php
@@ -1,81 +1,81 @@
-{{--  patient registration view: the component to capture patient details  --}}
+{{-- patient registration view: the component to capture patient details  --}}
 
 @extends('template')
 
 @section('content')
 <div class="section-light contact" id="contact">
-        <div class="container">
-          <div class="columns is-multiline" data-aos="fade-in-up" data-aos-easing="linear">
+    <div class="container">
+        <div class="columns is-multiline" data-aos="fade-in-up" data-aos-easing="linear">
             <div class="column is-12 about-me">
-              <h1 class="title has-text-centered section-title">
-                Get in touch
-              </h1>
+                <h1 class="title has-text-centered section-title">
+                    Get in touch
+                </h1>
             </div>
             <div class="column is-8 is-offset-2">
-              <form action="" method="POST">
-                <div class="columns">
-                            <div class="column is-half">
-                                <!-- Input:  First Name -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="first_name">First Name</label>
-                                        <input class="input" type="text" name="first_name" id="first_name">
-                                    </div>
+                <form action="" method="POST">
+                    <div class="columns">
+                        <div class="column is-half">
+                            <!-- Input:  First Name -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="first_name">First Name</label>
+                                    <input class="input" type="text" name="first_name" id="first_name">
                                 </div>
-                                <!-- Input:  Last Name -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="last_name">Last Name</label>
-                                        <input class="input" type="text" name="last_name" id="last_name">
-                                    </div>
+                            </div>
+                            <!-- Input:  Last Name -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="last_name">Last Name</label>
+                                    <input class="input" type="text" name="last_name" id="last_name">
                                 </div>
-                                <!-- Choice Select One:  Gender?.Sex -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="sex">Gender</label>
-                                        <div class="select">
-                                            <select name="sex" id="sex" class="regular-text">
-                                                <option value="U">Other</option>
-                                                <option value="F">Female</option>
-                                                <option value="M">Male</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Input:  Date of Birth -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="dob">Date of Birth</label>
-                                        <input class="input bulmaCalendar" type="date" name="dob" id="dob" data-display-mode="dialog">
+                            </div>
+                            <!-- Choice Select One:  Gender?.Sex -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="sex">Gender</label>
+                                    <div class="select">
+                                        <select name="sex" id="sex" class="regular-text">
+                                            <option value="U">Other</option>
+                                            <option value="F">Female</option>
+                                            <option value="M">Male</option>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
-                            <div class="column">
-                                <!-- Email Input:  Email Address -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="email">Email Address</label>
-                                        <input class="input" type="email" name="email" id="email" placeholder="patient@info.com">
-                                    </div>
-                                </div>
-                                <!-- Input:  Contact Details -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="last_name">Contact Number</label>
-                                        <input class="input" type="text" name="last_name" id="last_name">
-                                    </div>
-                                </div>
-                                <!-- Input:  Contact Details -->
-                                <div class="field">
-                                    <div class="control">
-                                        <label class="label" for="address">Address Details</label>
-                                        <textarea name="address" id="address" class="textarea"></textarea>
-                                    </div>
+                            <!-- Input:  Date of Birth -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="dob">Date of Birth</label>
+                                    <input class="input bulmaCalendar" type="date" name="dob" id="dob" data-display-mode="dialog">
                                 </div>
                             </div>
                         </div>
-              </form>
+                        <div class="column">
+                            <!-- Email Input:  Email Address -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="email">Email Address</label>
+                                    <input class="input" type="email" name="email" id="email" placeholder="patient@info.com">
+                                </div>
+                            </div>
+                            <!-- Input:  Contact Details -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="last_name">Contact Number</label>
+                                    <input class="input" type="text" name="last_name" id="last_name">
+                                </div>
+                            </div>
+                            <!-- Input:  Contact Details -->
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label" for="address">Address Details</label>
+                                    <textarea name="address" id="address" class="textarea"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
         </div>
-      </div>
+    </div>
+</div>
