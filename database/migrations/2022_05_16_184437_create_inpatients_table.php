a4a76cf92c322cada3007520efa709b00defc03b
Gift JR <muyembegift@gmail.com>
2022-06-26 00:59:34 +0200

    #6 Normalizing data types
    Optimization for vitals with focus on precision

--- database/migrations/2022_05_16_184437_create_inpatients_table.php
+++ database/migrations/2022_05_16_184437_create_inpatients_table.php
@@ -16,16 +16,17 @@ return new class extends Migration
         Schema::create('inpatients', function (Blueprint $table) {
             $table->bigIncrements('id')->index('inpatient_id');
             $table->unsignedBigInteger('patient_id');
-            $table->unsignedBigInteger('doctor_id')->nullable();
+            $table->unsignedBigInteger('doctor_id');
             $table->unsignedBigInteger('prescription_id');
-            $table->string('ward');
-            $table->string('diagnosis');
-            $table->string('blood_pressure');
-            $table->string('weight');
-            $table->string('height');
-            $table->string('temperature');
-            $table->string('visit_summary');
-            $table->integer('duration');
+            $table->string('ward', 16);
+            $table->string('diagnosis', 64);
+            $table->unsignedMediumInteger('bp_systolic'); //Millimetres of mercury (mmHg)
+            $table->unsignedMediumInteger('bp_diastolic'); //Millimetres of mercury (mmHg)
+            $table->decimal('weight', 3, 2); //Kilograms
+            $table->decimal('height', 3, 2)->nullable(); //Meters
+            $table->decimal('temperature', 2, 1); //Celsius (˚C)
+            $table->string('visit_summary', 128);
+            $table->unsignedMediumInteger('duration')->nullable();;
             $table->boolean('discharged');
             $table->date('discharged_date')->nullable();
             $table->timestamps();
--- database/migrations/2022_05_16_190002_create_outpatients_table.php
+++ database/migrations/2022_05_16_190002_create_outpatients_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -18,12 +17,19 @@ return new class extends Migration
             $table->unsignedBigInteger('patient_id');
             $table->unsignedBigInteger('doctor_id');
             $table->unsignedBigInteger('prescription_id');
-            $table->string('blood_pressure');
-            $table->string('weight');
-            $table->string('height')->nullable();
-            $table->string('temperature');
-            $table->string('diagnosis');
-            $table->string('reason_for_visit')->nullable();
+            /**
+             * Blood pressure is measured in millimetres of mercury (mmHg)
+             * and is given as two figures:
+             * i. systolic pressure – the pressure when your heart pushes blood out
+             * ii. diastolic pressure – the pressure when your heart rests between beats
+             */
+            $table->unsignedMediumInteger('bp_systolic'); //Millimetres of mercury (mmHg)
+            $table->unsignedMediumInteger('bp_diastolic'); //Millimetres of mercury (mmHg)
+            $table->decimal('weight', 3, 2); //Kilograms (kg)
+            $table->decimal('height', 3, 2)->nullable(); //Meters (m)
+            $table->decimal('temperature', 2, 1); //Celsius (˚C)
+            $table->string('diagnosis', 64);
+            $table->string('reason_for_visit', 128)->nullable(); //Patient complaints
             $table->timestamps();
             $table->softDeletes();
 
